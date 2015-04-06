a1ea7cfc21031c816712c807baf8f471833b5439
Akshay Deo <akshay@rainingclouds.com>
2015-04-06 18:30:09 +0530

    - added facebook comments widget

--- public/js/app.js
+++ public/js/app.js
@@ -62,6 +62,31 @@
             }
         };
     });
+    // facebook commetns directive
+    app.directive('dynFbCommentBox', function () {
+        function createHTML(href, numposts, colorscheme) {
+            return '<div class="fb-comments" ' +
+                'data-href="' + href + '" ' +
+                'data-numposts="' + numposts + '" ' +
+                'data-colorsheme="' + colorscheme + '">' +
+                '</div>';
+        }
+
+        return {
+            restrict: 'A',
+            scope: {},
+            link: function postLink(scope, elem, attrs) {
+                attrs.$observe('pageHref', function (newValue) {
+                    var href        = newValue;
+                    var numposts    = attrs.numposts    || 5;
+                    var colorscheme = attrs.colorscheme || 'light';
+
+                    elem.html(createHTML(href, numposts, colorscheme));
+                    FB.XFBML.parse(elem[0]);
+                });
+            }
+        };
+    });
     // adding http interceptor
     app.config(function ($httpProvider) {
         $httpProvider.interceptors.push('myHttpInterceptor');
